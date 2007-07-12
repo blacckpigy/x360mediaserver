8591cc62f44740222f8e1a262716065da4836b1b
NathanRobinson <NathanRobinson@d1e22a62-5d34-0410-94cd-e979f80d8836>
2007-07-12 06:51:03 +0000

    Initial import.

--- trunk/x360MediaServer/x360mediaserver/utils/StringUtils.java
+++ trunk/x360MediaServer/x360mediaserver/utils/StringUtils.java
@@ -1,11 +1,13 @@
 package x360mediaserver.utils;
 
-public class StringUtils {
-	public static String getHtmlString(String inputString){
-		String result=inputString.replace("&","&amp;");
-		result.replace("ö","&ouml;");
-		result.replace("ï","&iuml;");
-		
-		return result;
-	}
+public class StringUtils
+{
+    public static String getHtmlString(String inputString)
+    {
+        String result = inputString.replace("&", "&amp;");
+        result.replace("ö", "&ouml;");
+        result.replace("ï", "&iuml;");
+
+        return result;
+    }
 }
